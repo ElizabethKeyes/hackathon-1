61651662afed92a2ee3fb199583d1afb479e1204
Chandler Bowman <doctorgrant99@gmail.com>
2023-03-24 16:44:34 -0600

    styling modals

--- client/app/Models/Recipe.js
+++ client/app/Models/Recipe.js
@@ -15,7 +15,7 @@ export class Recipe {
     // TODO needs string interpolation for variables once the model has been defined (waiting to see how data comes back from the API)
     return `
     <div class="col-11">
-    <div class="list-card d-flex">
+    <div onclick="app.recipesController.setActiveRecipe('${this.id}')"  class="list-card d-flex selectable">
       <div>
         <img src="${this.imgUrl}"
           class="list-image" alt="recipe picture">
@@ -30,8 +30,26 @@ export class Recipe {
 
   get activeRecipeTemplate() {
     return `
-    
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">${this.title}</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <p>${this.ingredients}</p> <br>
+        ${this.description}
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
     `
   }
-
+  //NOTE - next getter should render like and dislike functionality
+  
 }
