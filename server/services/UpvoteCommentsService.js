09ceb589fab3db3da41841da83ff9a4b18a01d73
josh <joshjj3@gmail.com>
2023-03-24 17:52:39 -0600

    testing upvote exclusivity

--- server/services/UpvoteCommentsService.js
+++ server/services/UpvoteCommentsService.js
@@ -2,6 +2,10 @@ import { dbContext } from "../db/DbContext.js"
 
 class UpvoteCommentsService {
     async upvoteComment(upvoteData) {
+        const downVote = await dbContext.DownvoteComments.find({ commentId: upvoteData.commentId, userId: upvoteData.userId })
+        if (downVote) {
+            await dbContext.DownvoteComments.deleteMany(downVote)
+        }
         const newUpvote = await dbContext.UpvoteComments.create(upvoteData)
         return newUpvote
     }
