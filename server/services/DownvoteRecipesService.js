9f1668184261e80a17d5b60861a9f8019c955420
josh <joshjj3@gmail.com>
2023-03-24 23:04:13 -0600

    testing downvote recipes

--- server/services/DownvoteRecipesService.js
+++ server/services/DownvoteRecipesService.js
@@ -2,8 +2,13 @@ import { dbContext } from "../db/DbContext.js"
 
 class DownvoteRecipesService {
     async downvoteRecipe(downvoteData) {
+        const downvote = await dbContext.DownvoteRecipes.findById({ recipeId: downvoteData.recipeId, userId: downvoteData.userId })
+        if (downvote) {
+            const newDownvote = await dbContext.DownvoteRecipes.findOneAndDelete(downvote._id)
+        }
         const upVote = await dbContext.UpvoteRecipes.findOneAndDelete({
-            recipeId: downvoteData.recipeId, userId: downvoteData.userId })
+            recipeId: downvoteData.recipeId, userId: downvoteData.userId
+        })
         const newDownvote = await dbContext.DownvoteRecipes.create(downvoteData)
         return newDownvote
     }
