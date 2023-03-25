1bf6275909dc1a2415e32ec51b50f7a2d6aed6fd
Chandler Bowman <doctorgrant99@gmail.com>
2023-03-24 19:48:40 -0600

    upvotes and downvotes work and are exclusive bitch

--- server/services/UpvoteRecipesService.js
+++ server/services/UpvoteRecipesService.js
@@ -3,7 +3,7 @@ import { dbContext } from "../db/DbContext.js"
 
 class UpvoteRecipesService {
   async upvoteRecipe(upvoteData) {
-
+    const downVote = await dbContext.DownvoteRecipes.findOneAndDelete({ recipeId: upvoteData.recipeId, userId: upvoteData.userId })
     const newUpvote = await dbContext.UpvoteRecipes.create(upvoteData)
     return newUpvote
   }
