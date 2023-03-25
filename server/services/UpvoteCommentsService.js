a7c2526b1db3438ca12c63fb0f74bbe2ac6ebe90
josh <joshjj3@gmail.com>
2023-03-24 19:08:55 -0600

    user info populate

--- server/services/UpvoteCommentsService.js
+++ server/services/UpvoteCommentsService.js
@@ -2,10 +2,8 @@ import { dbContext } from "../db/DbContext.js"
 
 class UpvoteCommentsService {
     async upvoteComment(upvoteData) {
-        const downVote = await dbContext.DownvoteComments.find({ commentId: upvoteData.commentId, userId: upvoteData.userId })
-        if (downVote) {
-            await dbContext.DownvoteComments.deleteMany(downVote)
-        }
+        const downVote = await dbContext.DownvoteComments.findOneAndDelete({ commentId: upvoteData.commentId, userId: upvoteData.userId })
+
         const newUpvote = await dbContext.UpvoteComments.create(upvoteData)
         return newUpvote
     }
