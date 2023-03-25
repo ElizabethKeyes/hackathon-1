452f92fe0ccff26d58c9ae09d581f89da736399c
josh <joshjj3@gmail.com>
2023-03-24 18:11:11 -0600

    comment and recipes show profile pic and name

--- server/services/UpvoteRecipesService.js
+++ server/services/UpvoteRecipesService.js
@@ -3,6 +3,7 @@ import { dbContext } from "../db/DbContext.js"
 
 class UpvoteRecipesService {
   async upvoteRecipe(upvoteData) {
+
     const newUpvote = await dbContext.UpvoteRecipes.create(upvoteData)
     return newUpvote
   }
